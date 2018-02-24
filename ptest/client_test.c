b7b94cd953463f895d6c0b07db39d56c837b4cd0
Henry Corrigan-Gibbs <henrycg-git@cs.stanford.edu>
2018-02-24 13:12:36 -0800

    Test for client code

--- ptest/client_test.c
+++ ptest/client_test.c
@@ -39,3 +39,69 @@ mu_test_client__new (void)
   PrioConfig_clear (cfg);
 }
 
+void 
+test_client_agg (int nclients)
+{
+  PrioConfig cfg = PrioConfig_defaultNew();
+  mu_check (cfg);
+
+  const int ndata = PrioConfig_numDataFields (cfg);
+  bool data_items[ndata];
+  for (int i=0; i < ndata; i++) {
+    // Arbitrary data
+    data_items[i] = (i % 3 == 1) || (i % 5 == 3);
+  }
+
+  PrioServer sA = PrioServer_new (cfg);
+  PrioServer sB = PrioServer_new (cfg);
+  mu_check (sA);
+  mu_check (sB);
+
+  for (int i=0; i < nclients; i++) {
+    PrioPacketClient pA, pB;
+    mu_check (PrioPacketClient_new (cfg, data_items, &pA, &pB) == PRIO_OKAY);
+
+    mu_check (PrioServer_aggregate (sA, pA) == PRIO_OKAY);
+    mu_check (PrioServer_aggregate (sB, pB) == PRIO_OKAY);
+
+    PrioPacketClient_clear (pA);
+    PrioPacketClient_clear (pB);
+  }
+
+  PrioTotalShare tA = PrioServer_newTotalShare (sA);
+  PrioTotalShare tB = PrioServer_newTotalShare (sB);
+  mu_check (tA);
+  mu_check (tB);
+
+  unsigned long output[ndata];
+  mu_check (PrioTotalShare_final (cfg, output, tA, tB) == PRIO_OKAY);
+  for (int i=0; i < ndata; i++) {
+    unsigned long v = ((i % 3 == 1) || (i % 5 == 3));
+    mu_check (output[i] == v*nclients);
+  }
+
+  PrioTotalShare_clear (tA);
+  PrioTotalShare_clear (tB);
+
+  PrioServer_clear (sA);
+  PrioServer_clear (sB);
+  PrioConfig_clear (cfg);
+}
+
+void 
+mu_test_client__agg_1 (void)
+{
+  test_client_agg (1);
+}
+
+void 
+mu_test_client__agg_2 (void)
+{
+  test_client_agg (2);
+}
+
+void 
+mu_test_client__agg_1000 (void)
+{
+  test_client_agg (1000);
+}
